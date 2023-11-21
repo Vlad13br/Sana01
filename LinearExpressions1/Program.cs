ac7984c5910f5f01bc29a37ec1f351c403ea004b
Vlad_Barsuk <ipz231_bvp@student.ztu.edu.ua>
2023-11-21 21:24:16 +0200

    LinearExpressions1

--- LinearExpressions1/Program.cs
+++ LinearExpressions1/Program.cs
@@ -1,46 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+double a, b, c, d, x, y, z, r;
 
-namespace Sana01
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            double a, b, c, d, x, y, z, r;
-            string str;
-
-            Console.WriteLine("Введіть число A");
-            str = Console.ReadLine();
-            a = double.Parse(str);
+Console.WriteLine("Введіть число A");
+a = double.Parse(Console.ReadLine());
 
 
-            Console.WriteLine("Введіть число B");
-            str = Console.ReadLine();
-            b = double.Parse(str);
+Console.WriteLine("Введіть число B");
+b = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Введіть число C");
-            str = Console.ReadLine();
-            c = double.Parse(str);
+Console.WriteLine("Введіть число C");
+c = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Введіть число D");
-            str = Console.ReadLine();
-            d = double.Parse(str);
-            x = (a + 2 * b - c + d) / c * d + (a + b) / (c - d) - (a * a) / (b * b);
-            Console.WriteLine("Результат обчислення виразу x = " + x);
+Console.WriteLine("Введіть число D");
+d = double.Parse(Console.ReadLine());
 
-            y = (5 * (a + b) * (c - d)) / (0.5 * c) + d * d * ((a * a - b * b) / (b - a));
-            Console.WriteLine("Результат обчислення виразу y = " + y);
+x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - Math.Pow(a, 2) / Math.Pow(b, 2);
+Console.WriteLine("Результат обчислення виразу x = " + x);
 
-            z = ((Math.Pow((x * x - 2 * x), 3) - 4 * (x * x * x * x + 1) * (1 - b)) / (5 * a + 3 * b));
-            Console.WriteLine("Результат обчислення виразу z = " + z);
+y = (5 * (a + b) * (c - d)) / (0.5 * c) + Math.Pow(d, 2) * ((Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a));
+Console.WriteLine("Результат обчислення виразу y = " + y);
 
-            r = ((0.5 * a + 0.75 * b - 7 / 5) / 3 * c + 1) + 1 / (a - c);
-            Console.WriteLine("Результат обчислення виразу r = " + r);
+z = ((Math.Pow(Math.Pow(x, 2) - 2 * x, 3) - 4 * (Math.Pow(x, 4) + 1) * (1 - b)) / (5 * a + 3 * b));
+Console.WriteLine("Результат обчислення виразу z = " + z);
 
-        }
-    }
-}
+r = (0.5 * a + 0.75 * b - 7 / 5) / (3 * c + 1) + 1 / (a - c);
+Console.WriteLine("Результат обчислення виразу r = " + r);
